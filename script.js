9fbb59af81280ec9a73098d442e361b37fc6fc17
LarsGJobloop <lars.gunnar@jobloop.no>
2024-08-28 13:27:44 +0200

    add example of a game loop

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
-let greeting = "Hello Kodeloftet"
-
-console.log(greeting)
-greeting = "Helt annet"
-console.log(greeting)
-
-// dette er en kommentar
-
-let a = 10
-let b = 32
-let c = a + b
-console.log(c)
-
-let lars = {
-    name: "Lars Gunnar",
-    age: 35,
-    alive: false,
+let player = {
+    name: "William",
+    health: 10,
 }
-console.log(lars)
 
-console.log(lars.name)
-
-
-if (lars.alive) 
+while (player.health > 0)
 {
-    console.log("He's alive!")
+    // Handle updating the player's state
+    let random = Math.random()
+    if (random < 0.5) {
+        console.log("Player taking damage")
+        player.health = player.health - 1
+    }
+
+    // Update the display
+    console.log("Player Health:")
+    console.log(player.health)
 }
-else
-{
-    console.log("He's dead!")
-}
 
-
-
-
-
-
-
-
-
+console.log("Player is dead")
