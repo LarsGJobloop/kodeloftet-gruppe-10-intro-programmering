fabd3b8e67cd58d3d029161fdeb123249d590177
LarsGJobloop <lars.gunnar@jobloop.no>
2024-08-28 12:09:55 +0200

    add introductionary examples

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log('Hello!');
+let greeting = "Hello Kodeloftet"
+
+console.log(greeting)
+greeting = "Helt annet"
+console.log(greeting)
+
+// dette er en kommentar
+
+let a = 10
+let b = 32
+let c = a + b
+console.log(c)
+
+let lars = {
+    name: "Lars Gunnar",
+    age: 35,
+    alive: false,
+}
+console.log(lars)
+
+console.log(lars.name)
+
+
+if (lars.alive) 
+{
+    console.log("He's alive!")
+}
+else
+{
+    console.log("He's dead!")
+}
+
+
+
+
+
+
+
+
+
